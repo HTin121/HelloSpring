f12eaa5a0ec4cb919ffe5f3f97d18f6fe61ca08d
hovantin <hvtin1110@gmail.com>
2025-08-14 17:10:09 +0700

    update

--- src/main/java/com/SpringBoot/helloworld/HelloController.java
+++ src/main/java/com/SpringBoot/helloworld/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String getHello() {
-        return "hello world! hi hi";
+        return "hello world! haha";
     }
 
 }
