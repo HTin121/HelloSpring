266cd25ffa10ae65a8087979252f3489b29bf0ef
hovantin <hvtin1110@gmail.com>
2025-08-14 16:58:23 +0700

    init project

---
+++ src/main/java/com/SpringBoot/helloworld/HelloController.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String getHello() {
+        return "hello world! hi hi";
+    }
+
+}
